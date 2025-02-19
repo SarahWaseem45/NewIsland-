059e0921baf245954e615e96ebdf17de249b3dc9
SarahWaseem45 <sarahwaseem40@gmail.com>
2025-02-19 10:50:15 +0500

    pause

--- Assets/Scritps/SceneChange.cs
+++ Assets/Scritps/SceneChange.cs
@@ -11,8 +11,36 @@ public class SceneChange : MonoBehaviour
     {
         SceneManager.LoadScene("Level 2"); 
     }
-     public void MathScene()
+     public void EasyMath()
     {
-        SceneManager.LoadScene("Game"); 
+        SceneManager.LoadScene("Easy Math"); 
+    }
+     public void SpellingScene()
+    {
+        SceneManager.LoadScene("spell game"); 
+    }
+     public void Level3Scene()
+    {
+        SceneManager.LoadScene("level 3"); 
+    }
+     public void MediumMath()
+    {
+        SceneManager.LoadScene("Medium Math"); 
+    }
+    public void HardMath()
+    {
+        SceneManager.LoadScene("Hard Math"); 
+    }
+     public void EasySpellGame()
+    {
+        SceneManager.LoadScene("EasySpellGame"); 
+    }
+     public void MediumSpellGame()
+    {
+        SceneManager.LoadScene("MediumSpellGame"); 
+    }
+     public void HardSpellGame()
+    {
+        SceneManager.LoadScene("HardSpellGame"); 
     }
 }
